38307ca9e40ac43fd5bbbc7d3ab6136c2d3befcc
cloprothro@gmail.com <clostar.delta@gmail.com>
2024-06-26 11:35:15 -0700

    finished up exercise for 2.3 adding useEffect

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import SearchFilter from './components/SearchFilter'
 import DisplayPeople from './components/DisplayPeople'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1, invisible: false},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2, invisible: false },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3, invisible: false },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4, invisible: false }
-  ])
+  const [persons, setPersons] = useState([])
+
+  const hook = () => {
+    const people = []
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        response.data.map((x) => people.push(x))
+        console.log(people)
+        setPersons(people)
+      })
+  }
+  useEffect(hook, [])
+
   const [formData, setFormData] = useState ({
     name: "",
     number: ""
