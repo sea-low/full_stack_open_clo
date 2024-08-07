7eb00dd33024004c51476cf6572ec6223f77c501
cloprothro@gmail.com <clostar.delta@gmail.com>
2024-08-07 15:34:10 -0700

    did e3.9-3.11 looks like PUT needs some love but next section ig

--- part2/phonebook/services/phonebook.js
+++ part2/phonebook/services/phonebook.js
@@ -1,29 +1,29 @@
-import axios from "axios"
-const baseUrl = 'http://localhost:3001/persons'
+import axios from "axios";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
 const create = (newPerson) => {
-    const personDTO = {
-        name: newPerson.name, 
-        number: newPerson.number, 
-        id: newPerson.id
-    }
-    const request = axios.post(baseUrl, personDTO)
-    return request.then(response => response.data)
-}
+  const personDTO = {
+    name: newPerson.name,
+    number: newPerson.number,
+    id: newPerson.id,
+  };
+  const request = axios.post(baseUrl, personDTO);
+  return request.then((response) => response.data);
+};
 
 const update = (person, newPerson) => {
-    const request = axios.put(`${baseUrl}/${person.id}`, newPerson)
-    return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${person.id}`, newPerson);
+  return request.then((response) => response.data);
+};
 
 const eliminate = (person) => {
-    const request = axios.delete(`${baseUrl}/${person.id}`)
-    return request.then(response => response.data)
-}
+  const request = axios.delete(`${baseUrl}/${person.id}`);
+  return request.then((response) => response.data);
+};
 
-export default { getAll, create, update, eliminate }
+export default { getAll, create, update, eliminate };
